540399da1e17c92ba9bf1d6cef47f3016d64d9f1
ginanjarsumantri <ginanjar.sumantri@agungsedayu.com>
2023-04-13 15:17:48 +0700

    -
    Signed-off-by: ginanjarsumantri <ginanjar.sumantri@agungsedayu.com>

--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from routes.endpoints import bidang, bidangoverlap, desa, planing, project, ptsk, rincik, section, jenis_lahan
+from routes.endpoints import bidang, bidangoverlap, desa, planing, project, ptsk, rincik, section, jenis_lahan, draft
 
 api_router = APIRouter()
 api_router.include_router(bidang.router, prefix="/bidang", tags=["bidang"])
@@ -11,3 +11,4 @@ api_router.include_router(ptsk.router, prefix="/ptsk", tags=["ptsk"])
 api_router.include_router(rincik.router, prefix="/rincik", tags=["rincik"])
 api_router.include_router(section.router, prefix="/section", tags=["section"])
 api_router.include_router(jenis_lahan.router, prefix="/jenislahan", tags=["jenislahan"])
+api_router.include_router(draft.router, prefix="/draft", tags=["draft"])
