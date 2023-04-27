4114cb11f48e394f1ec9198b872f8d2f95cb7882
ginanjarsumantri <ginanjar.sumantri@agungsedayu.com>
2023-04-27 15:44:34 +0700

    router skpt ketinggalan
    Signed-off-by: ginanjarsumantri <ginanjar.sumantri@agungsedayu.com>

--- routes/api.py
+++ routes/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from routes.endpoints import bidang, bidangoverlap, desa, planing, project, ptsk, rincik, section, jenis_lahan, draft, gps
+from routes.endpoints import (bidang, bidangoverlap, desa, planing, project, 
+                              ptsk, rincik, section, jenis_lahan, draft, gps, skpt)
 
 api_router = APIRouter()
 api_router.include_router(bidang.router, prefix="/bidang", tags=["bidang"])
@@ -8,6 +9,7 @@ api_router.include_router(desa.router, prefix="/desa", tags=["desa"])
 api_router.include_router(planing.router, prefix="/planing", tags=["planing"])
 api_router.include_router(project.router, prefix="/project", tags=["project"])
 api_router.include_router(ptsk.router, prefix="/ptsk", tags=["ptsk"])
+api_router.include_router(skpt.router, prefix="/skpt", tags=["skpt"])
 api_router.include_router(rincik.router, prefix="/rincik", tags=["rincik"])
 api_router.include_router(section.router, prefix="/section", tags=["section"])
 api_router.include_router(jenis_lahan.router, prefix="/jenislahan", tags=["jenislahan"])
