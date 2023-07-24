3d99d9d0c855dfff7d548d7665f2be495315aac2
ginanjar <ginanjar.sumantri@agungsedayu.com>
2023-07-24 17:33:51 +0700

    -

--- models/bundle_model.py
+++ models/bundle_model.py
@@ -60,7 +60,6 @@ class BundleDtBase(SQLModel):
     bundle_hd_id:UUID | None = Field(default=None, foreign_key="bundle_hd.id", nullable=False)
     dokumen_id:UUID | None = Field(default=None, foreign_key="dokumen.id", nullable=False)
     file_path:str | None = Field(nullable=True)
-    file_type:str | None = Field(nullable=True)
 
 class BundleDtFullBase(BaseUUIDModel, BundleDtBase):
     pass
