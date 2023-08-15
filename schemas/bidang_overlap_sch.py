d371904f154f97369dbfe6cfda171bfd9b08e65a
ginanjar <ginanjar.sumantri@agungsedayu.com>
2023-08-15 11:47:03 +0700

    get_all

---
+++ schemas/bidang_overlap_sch.py
@@ -0,0 +1,19 @@
+from models.bidang_overlap_model import BidangOverlap, BidangOverlapBase, BidangOverlapFullBase, BidangOverlapRawBase
+from common.partial import optional
+from common.as_form import as_form
+from sqlmodel import Field
+
+
+class BidangOverlapCreateSch(BidangOverlapBase):
+    pass
+
+class BidangOverlapRawSch(BidangOverlapRawBase):
+    pass
+
+class BidangOverlapSch(BidangOverlapFullBase):
+    pass
+
+@as_form
+@optional
+class BidangOverlapUpdateSch(BidangOverlapBase):
+    pass
