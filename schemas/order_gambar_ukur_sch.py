356709c96b35df29d198169905167f507d45fa38
ginanjar <ginanjar.sumantri@agungsedayu.com>
2023-09-14 16:23:31 +0700

    -

--- schemas/order_gambar_ukur_sch.py
+++ schemas/order_gambar_ukur_sch.py
@@ -9,7 +9,7 @@ from uuid import UUID
 from decimal import Decimal
 
 class OrderGambarUkurCreateSch(OrderGambarUkurBase):
-    kjb_dts:list[UUID]
+    bidangs:list[UUID]
     tembusans:list[UUID]
 
 
