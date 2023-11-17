14c973912000d03058c1f3c5574baa8e707cb991
ginanjar <ginanjar.sumantri@agungsedayu.com>
2023-11-17 15:10:53 +0700

    -

--- models/marketing_model.py
+++ models/marketing_model.py
@@ -20,6 +20,7 @@ class Manager(ManagerFullBase, table=True):
 
 
 class SalesBase(SQLModel):
+    manager_id:UUID | None = Field(foreign_key="manager.id")
     name:str | None = Field(nullable=False, max_length=200)
     is_active:bool
 
@@ -27,8 +28,6 @@ class SalesFullBase(BaseUUIDModel, SalesBase):
     pass
 
 class Sales(SalesFullBase, table=True):
-    manager_id:UUID | None = Field(foreign_key="manager.id")
-
     manager:"Manager" = Relationship(back_populates="salests", sa_relationship_kwargs={'lazy':'selectin'})
     # kjb_hds: list["KjbHd"] = Relationship(back_populates="sales", sa_relationship_kwargs={'lazy':'select'})
 
