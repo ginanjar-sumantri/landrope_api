0dfe84226dc9fe103c8b0721932881d141464387
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-01-30 16:39:54 +0700

    report tim ukur

--- routes/endpoints/report.py
+++ routes/endpoints/report.py
@@ -9,7 +9,7 @@ from schemas.response_sch import (GetResponseBaseSch, GetResponsePaginatedSch, c
 from common.rounder import RoundTwo
 from decimal import Decimal
 from openpyxl import Workbook
-from openpyxl.styles import Font
+from openpyxl.styles import Font, PatternFill
 from openpyxl.writer.excel import save_workbook
 from datetime import date
 from io import BytesIO
@@ -29,12 +29,20 @@ async def report_tim_ukur(start_date:date, end_date:date):
     header_string = ["No", "No. Permintaan Ukur", "Tanggal Terima Berkas", "Mediator", "Group", "Desa", "Nama Pemilik Tanah", "Jenis Surat (GIRIK/SHM)", 
                     "Alas Hak", "Luas Surat (m2)", "Tanggal Pengukuran", "Penunjuk Batas", "Luas Ukur (m2)", "Surveyor", "Tanggal Kirim Ukur ke Analis", "Keterangan"]
     
+    end_column = len(header_string)
 
     ws.cell(row=1, column=2, value="LAPORAN PENGUKURAN")
+    ws.merge_cells(start_row=1, start_column=2, end_row=1, end_column=4)
     ws.cell(row=2, column=2, value=f"CUT OFF DATE {start_date} S/D {end_date}")
+    ws.merge_cells(start_row=2, start_column=2, end_row=2, end_column=4)
 
     for idx, val in enumerate(header_string):
-        ws.cell(row=1, column=idx + 1, value=val).font = Font(bold=True)
+        ws.cell(row=3, column=idx + 1, value=val).font = Font(bold=True)
+        if idx + 1 < 11:
+            ws.cell(row=3, column=idx + 1, value=val).fill = PatternFill(start_color="00FFFF00", end_color="00FFFF00", fill_type="solid")
+        else:
+            ws.cell(row=3, column=idx + 1, value=val).fill = PatternFill(start_color="00C0C0C0", end_color="00C0C0C0", fill_type="solid")
+
 
     if start_date:
             query_start_date = "rpl.tanggal_terima_berkas >="f'"{start_date}"'
@@ -65,14 +73,14 @@ async def report_tim_ukur(start_date:date, end_date:date):
             LEFT OUTER JOIN desa d ON d.id = dt.desa_by_ttn_id
             LEFT OUTER JOIN pemilik p ON p.id = dt.pemilik_id
             LEFT OUTER JOIN keterangan_req_petlok ket ON ket.id = rpl.keterangan_req_petlok_id
-            --WHERE ({query_start_date} {query_end_date})
+            WHERE ({query_start_date} {query_end_date})
     """
 
     db_session = db.session
     response = await db_session.execute(query)
     result = response.all()
 
-    x = 1
+    x = 3
     for row_data in result:
         x += 1
         ws.cell(row=x, column=1, value=row_data[0])
@@ -93,12 +101,19 @@ async def report_tim_ukur(start_date:date, end_date:date):
         ws.cell(row=x, column=16, value=row_data[15])
     
     ws.cell(row=x + 2, column=2, value="CATATAN:")
+    ws.merge_cells(start_row=x + 2, start_column=2, end_row=x + 2, end_column=end_column)
     ws.cell(row=x + 3, column=2, value="1. KOLOM B S/D J DIPEROLEH DARI TIM MARKETING (DARI REQUEST UKUR/PETA LOKASI)")
+    ws.merge_cells(start_row=x + 3, start_column=2, end_row=x + 3, end_column=end_column)
     ws.cell(row=x + 4, column=2, value="2. KOLOM K S/D P DIISI OLEH ADMIN UKUR DI SISTEM, SETELAH MENERIMA HASIL UKUR")
+    ws.merge_cells(start_row=x + 4, start_column=2, end_row=x + 4, end_column=end_column)
     ws.cell(row=x + 5, column=2, value="3. 'TANGGAL PENGUKURAN' ADALAH TANGGAL TIM UKUR MELAKUKAN PENGUKURAN DI LAPANGAN ")
+    ws.merge_cells(start_row=x + 5, start_column=2, end_row=x + 5, end_column=end_column)
     ws.cell(row=x + 6, column=2, value="4. 'TANGGAL KIRIM UKUR KE ANALIS' ADALAH TANGGAL TIM UKUR MENGIRIMKAN HASIL UKUR KE ANALIS")
+    ws.merge_cells(start_row=x + 6, start_column=2, end_row=x + 6, end_column=end_column)
     ws.cell(row=x + 7, column=2, value="5. 'KETERANGAN' DIISI DENGAN INFORMASI DARI TIM UKUR APABILA ADA KENDALA/PENDING SEPERTI : BELUM UKUR MENUNGGU INFO MEDIATOR, PENJUAL BELUM MENUNJUKKAN BIDANG, DLL")
+    ws.merge_cells(start_row=x + 7, start_column=2, end_row=x + 7, end_column=end_column)
     ws.cell(row=x + 8, column=2, value="6. 'TANGGAL TERIMA BERKAS' ADALAH TANGGAL TIM UKUR MENERIMA BERKAS UKUR DARI MARKETING")
+    ws.merge_cells(start_row=x + 8, start_column=2, end_row=x + 8, end_column=end_column)
     
 
     excel_data = BytesIO()
