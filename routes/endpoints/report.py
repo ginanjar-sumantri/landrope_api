a2e6904559fd814599eef389420fad8a606f0a9a
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-02-01 10:41:17 +0700

    report summary analisa

--- routes/endpoints/report.py
+++ routes/endpoints/report.py
@@ -11,7 +11,7 @@ from schemas.response_sch import (GetResponseBaseSch, GetResponsePaginatedSch, c
 from common.rounder import RoundTwo
 from decimal import Decimal
 from openpyxl import Workbook
-from openpyxl.styles import Font, PatternFill
+from openpyxl.styles import Font, PatternFill, Alignment
 from openpyxl.writer.excel import save_workbook
 from datetime import date
 from io import BytesIO
@@ -278,7 +278,7 @@ async def report_hasil_analisa(background_task:BackgroundTasks, sch:HasiLPetaLok
     wb = Workbook()
     ws = wb.active
 
-    ws.title =  "REPORT TIM UKUR"
+    ws.title =  "REPORT HASIL ANALISA"
     ws.firstHeader
 
     ws.cell(row=1, column=2, value="HASIL ANALISA")
@@ -360,6 +360,7 @@ async def report_hasil_analisa(background_task:BackgroundTasks, sch:HasiLPetaLok
                              media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
                              headers={"Content-Disposition": "attachment; filename=generated_excel.xlsx"})
 
+
 async def update_tanggal_kirim_berkas(sch:HasiLPetaLokasiUpdateTanggalKirimBerkasSch):
      
     query = update(HasilPetaLokasi).where(HasilPetaLokasi.id.in_(sch.ids)).values(tanggal_kirim_berkas=sch.tanggal_kirim_berkas)
@@ -367,3 +368,214 @@ async def update_tanggal_kirim_berkas(sch:HasiLPetaLokasiUpdateTanggalKirimBerka
     db_session = db.session
     await db_session.execute(query)
     await db_session.commit()
+
+
+@router.get("/summary-analyst")
+async def report_summary_analyst(start_date:date, end_date:date):
+
+    wb = Workbook()
+    ws = wb.active
+
+    ws.title =  "REPORT TIM UKUR"
+    ws.firstHeader
+
+    ws.cell(row=1, column=2, value="LAPORAN PENGUKURAN")
+    ws.merge_cells(start_row=1, start_column=2, end_row=1, end_column=6)
+    ws.cell(row=2, column=2, value=f"CUT OFF DATE {start_date} S/D {end_date}")
+    ws.merge_cells(start_row=2, start_column=2, end_row=2, end_column=6)
+
+    ws.cell(row=3, column=8, value="Laporan Ukuran")
+    ws.cell(row=3, column=8, value="Laporan Ukuran").font = Font(bold=True)
+    ws.cell(row=3, column=8, value="Laporan Ukuran").fill = PatternFill(start_color="00C0C0C0", end_color="00C0C0C0", fill_type="solid")
+    ws.cell(row=3, column=8, value="Laporan Ukuran").alignment = Alignment(horizontal='center', vertical='center')
+    ws.merge_cells(start_row=3, start_column=8, end_row=3, end_column=11)
+
+    ws.cell(row=3, column=12, value=f"Laporan Analis")
+    ws.cell(row=3, column=12, value="Laporan Analis").font = Font(bold=True)
+    ws.cell(row=3, column=12, value="Laporan Analis").fill = PatternFill(start_color="00C0C0C0", end_color="00C0C0C0", fill_type="solid")
+    ws.cell(row=3, column=12, value="Laporan Analis").alignment = Alignment(horizontal='center', vertical='center')
+    ws.merge_cells(start_row=3, start_column=12, end_row=3, end_column=15)
+
+    r4c8 = ws.cell(row=4, column=8)
+    r4c8.font = Font(bold=True)
+    r4c8.fill = PatternFill(start_color="0099CC00", end_color="0099CC00", fill_type="solid")
+    r4c8.alignment = Alignment(horizontal='center', vertical='center')
+    r4c8.value = "Sudah Ukur"
+    ws.merge_cells(start_row=4, start_column=8, end_row=4, end_column=9)
+
+    r4c10 = ws.cell(row=4, column=10)
+    r4c10.font = Font(bold=True)
+    r4c10.fill = PatternFill(start_color="00993366", end_color="00993366", fill_type="solid")
+    r4c10.alignment = Alignment(horizontal='center', vertical='center')
+    r4c10.value = "Belum Ukur"
+    ws.merge_cells(start_row=4, start_column=10, end_row=4, end_column=11)
+
+    r4c12 = ws.cell(row=4, column=12)
+    r4c12.font = Font(bold=True)
+    r4c12.fill = PatternFill(start_color="0099CC00", end_color="0099CC00", fill_type="solid")
+    r4c12.alignment = Alignment(horizontal='center', vertical='center')
+    r4c12.value = "Dibeli"
+    ws.merge_cells(start_row=4, start_column=12, end_row=4, end_column=13)
+
+    r4c14 = ws.cell(row=4, column=14)
+    r4c14.font = Font(bold=True)
+    r4c14.fill = PatternFill(start_color="00993366", end_color="00993366", fill_type="solid")
+    r4c14.alignment = Alignment(horizontal='center', vertical='center')
+    r4c14.value = "Tidak Dibeli"
+    ws.merge_cells(start_row=4, start_column=14, end_row=4, end_column=15)
+
+    header_string = ["Nomor KJB", "Desa", "Group", "Manager", "No Permintaan Ukur", "Jumlah Bidang", "Total Luas (m2)",
+                    "Bidang", "Luas (m2)", "Bidang", "Luas (m2)", "Bidang", "Luas (m2)", "Bidang", "Luas (m2)", "Status"]
+    
+    end_column = len(header_string)
+
+    for idx, val in enumerate(header_string):
+        ws.cell(row=5, column=idx + 1, value=val).font = Font(bold=True)
+        ws.cell(row=5, column=idx + 1, value=val).fill = PatternFill(start_color="00C0C0C0", end_color="00C0C0C0", fill_type="solid")
+
+    if start_date:
+        query_start_date = "rpl.tanggal_terima_berkas >=" f"'{start_date}'"
+    if end_date:
+        query_end_date = " AND rpl.tanggal_terima_berkas <" f"'{end_date}'"
+
+    query = f"""
+            WITH subquery AS (SELECT
+            hd.code AS nomor_kjb,
+            d.name AS desa_name,
+            dt.group,
+            m.name AS manager_name,
+            rpl.code AS nomor_request,
+            COUNT(rpl.kjb_dt_id) AS jumlah_bidang,
+            SUM(dt.luas_surat_by_ttn) AS total_luas,
+            COALESCE((SELECT COUNT(rpl_.id) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            WHERE rpl_.code = rpl.code 
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')
+            AND rpl_.luas_ukur IS NOT NULL 
+            AND rpl_.tanggal_pengukuran IS NOT NULL), 0) AS sudah_diukur,
+            COALESCE((SELECT SUM(rpl_.luas_ukur) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            WHERE rpl_.code = rpl.code 
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')
+            AND rpl_.luas_ukur IS NOT NULL 
+            AND rpl_.tanggal_pengukuran IS NOT NULL), 0) AS luas_sudah_diukur,
+            COALESCE((SELECT COUNT(rpl_.id) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            WHERE rpl_.code = rpl.code
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')
+            AND rpl_.luas_ukur IS NULL 
+            AND rpl_.tanggal_pengukuran IS NULL), 0) AS belum_diukur,
+            COALESCE((SELECT SUM(dt_.luas_surat_by_ttn) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            WHERE rpl_.code = rpl.code 
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')
+            AND rpl_.luas_ukur IS NULL 
+            AND rpl_.tanggal_pengukuran IS NULL), 0) AS luas_belum_diukur,
+            COALESCE((SELECT COUNT(rpl_.id) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            INNER JOIN hasil_peta_lokasi hpl_ ON rpl_.id = hpl_.request_peta_lokasi_id
+            WHERE rpl_.code = rpl.code 
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')), 0) AS dibeli,
+            COALESCE((SELECT SUM(hpl_.luas_surat) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            INNER JOIN hasil_peta_lokasi hpl_ ON rpl_.id = hpl_.request_peta_lokasi_id
+            WHERE rpl_.code = rpl.code 
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')), 0) AS luas_dibeli,
+            COALESCE((SELECT COUNT(rpl_.id) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            LEFT OUTER JOIN hasil_peta_lokasi hpl_ ON rpl_.id = hpl_.request_peta_lokasi_id
+            WHERE rpl_.code = rpl.code 
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')
+            AND hpl_.id IS NULL), 0) AS tidak_dibeli,
+            COALESCE((SELECT SUM(hpl_.luas_surat) 
+            FROM request_peta_lokasi rpl_ 
+            INNER JOIN kjb_dt dt_ ON dt_.id = rpl_.kjb_dt_id
+            INNER JOIN hasil_peta_lokasi hpl_ ON rpl_.id = hpl_.request_peta_lokasi_id
+            WHERE rpl_.code = rpl.code 
+            AND d.id = dt_.desa_by_ttn_id 
+            AND COALESCE(dt.group, '') = COALESCE(dt_.group, '')
+            AND hpl_.id IS NULL), 0) AS luas_tidak_dibeli
+            FROM request_peta_lokasi rpl
+            INNER JOIN kjb_dt dt ON dt.id = rpl.kjb_dt_id
+            INNER JOIN kjb_hd hd ON hd.id = dt.kjb_hd_id
+            LEFT OUTER JOIN desa d ON d.id = dt.desa_by_ttn_id
+            LEFT OUTER JOIN manager m ON m.id = hd.manager_id
+            WHERE {query_start_date} {query_end_date}
+            GROUP BY hd.code, d.id, dt.group, m.name, rpl.code)
+            SELECT s.*,
+            CASE
+                WHEN sudah_diukur >= jumlah_bidang THEN 'ORDER UKUR SELESAI'
+                ELSE 'ORDER UKUR BELUM SELESAI'
+            END AS status
+            FROM subquery s
+            ORDER BY nomor_request ASC
+    """
+
+    db_session = db.session
+    response = await db_session.execute(query)
+    result = response.all()
+
+    x = 5
+    for row_data in result:
+        x += 1
+        ws.cell(row=x, column=1, value=row_data[0])
+        ws.cell(row=x, column=2, value=row_data[1])
+        ws.cell(row=x, column=3, value=row_data[2])
+        ws.cell(row=x, column=4, value=row_data[3])
+        ws.cell(row=x, column=5, value=row_data[4])
+        ws.cell(row=x, column=6, value=row_data[5])
+        ws.cell(row=x, column=7, value=row_data[6])
+        ws.cell(row=x, column=8, value=row_data[7])
+        ws.cell(row=x, column=9, value=row_data[8])
+        ws.cell(row=x, column=10, value=row_data[9])
+        ws.cell(row=x, column=11, value=row_data[10])
+        ws.cell(row=x, column=12, value=row_data[11])
+        ws.cell(row=x, column=13, value=row_data[12])
+        ws.cell(row=x, column=14, value=row_data[13])
+        ws.cell(row=x, column=15, value=row_data[14])
+        ws.cell(row=x, column=16, value=row_data[15])
+    
+    total_order_selesai = RoundTwo(sum([data[6] for data in result if data[15] == "ORDER UKUR SELESAI"])/10000)
+    total_order_belum_selesai = RoundTwo(sum([data[6] for data in result if data[15] == "ORDER UKUR BELUM SELESAI"])/10000)
+
+    ws.cell(row=x + 3, column=2, value=f"TOTAL ORDER SELESAI: {total_order_selesai}")
+    ws.merge_cells(start_row=x + 3, start_column=2, end_row=x + 3, end_column=end_column)
+    ws.cell(row=x + 4, column=2, value=f"TOTAL ORDER BELUM SELESAI: {total_order_belum_selesai}")
+    ws.merge_cells(start_row=x + 4, start_column=2, end_row=x + 4, end_column=end_column)
+
+    
+    ws.cell(row=x + 6, column=2, value="CATATAN:")
+    ws.merge_cells(start_row=x + 6, start_column=2, end_row=x + 6, end_column=end_column)
+    ws.cell(row=x + 7, column=2, value="1. ORDER UKUR SELESAI JIKA SUDAH JADI PETA LOKASI")
+    ws.merge_cells(start_row=x + 7, start_column=2, end_row=x + 7, end_column=end_column)
+    ws.cell(row=x + 8, column=2, value="2. JIKA BIDANG SUDAH DIUKUR, NAMUN BELUM JADI PETA LOKASI MAKA MASIH MENJADI OUTSTANDING ORDER UKUR")
+    ws.merge_cells(start_row=x + 8, start_column=2, end_row=x + 8, end_column=end_column)
+    
+    
+
+    excel_data = BytesIO()
+
+    # Simpan workbook ke objek BytesIO
+    wb.save(excel_data)
+
+    # Set posisi objek BytesIO ke awal
+    excel_data.seek(0)
+    
+
+    return StreamingResponse(excel_data,
+                             media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
+                             headers={"Content-Disposition": "attachment; filename=generated_excel.xlsx"})
