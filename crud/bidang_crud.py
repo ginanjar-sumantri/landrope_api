20a013ec62d8033d3bda0e3213a254fb216f31c9
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-02-20 11:00:37 +0700

    bug pt penampung not in selectinload

--- crud/bidang_crud.py
+++ crud/bidang_crud.py
@@ -73,6 +73,7 @@ class CRUDBidang(CRUDBase[Bidang, BidangCreateSch, BidangUpdateSch]):
                         ).options(selectinload(Bidang.kategori)
                         ).options(selectinload(Bidang.kategori_sub)
                         ).options(selectinload(Bidang.kategori_proyek)
+                        ).options(selectinload(Bidang.penampung)
                         ).options(selectinload(Bidang.skpt).options(selectinload(Skpt.ptsk))
                         ).options(selectinload(Bidang.manager)
                         ).options(selectinload(Bidang.sales)
