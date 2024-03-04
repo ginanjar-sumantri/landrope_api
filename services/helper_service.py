20a40f0255d34120795aa2a1d53543291441ab49
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-03-04 17:31:38 +0700

    replace symbol /

--- services/helper_service.py
+++ services/helper_service.py
@@ -470,7 +470,7 @@ class BundleHelper:
         bytes_data = base64.b64decode(base64_str)
         file_type = filetype.guess(bytes_data)
         file_extension = str(file_type.extension)
-        file_name = f"Bundle-{key_value}-{uuid.uuid4().hex}"
+        file_name = f"Bundle-{key_value.replace('/', '_')}-{uuid.uuid4().hex}"
         binary_io_data = io.BytesIO(bytes_data)
 
         file = UploadFile(file=binary_io_data, filename=f"{file_name}.{file_extension}")
