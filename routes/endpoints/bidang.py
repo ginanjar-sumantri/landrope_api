6b0e318f65f1dd04f83fadc959a23a165025f5ea
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-06-28 11:19:12 +0700

    test get all data for shp

--- routes/endpoints/bidang.py
+++ routes/endpoints/bidang.py
@@ -711,15 +711,17 @@ async def export_shp(
     else:
         raise HTTPException(status_code=422, detail="Failed Export, please contact administrator!")
     
-@router.post("/export/bulk/shp/stream")
+@router.post("/export/bulk/shp/stream", response_model=GetResponseBaseSch[list[BidangShpSch]])
 async def export_shp(
             param:BidangParameterDownload|None,
             bg_task:BackgroundTasks,
             current_worker:Worker = Depends(crud.worker.get_active_worker)):
+    
+    results = await crud.bidang.get_multi_export_shape_file(param=param)
 
-    await BidangService().create_export_log_with_generate_file_shp(param=param, created_by_id=current_worker.id, bg_task=bg_task)
+    # await BidangService().create_export_log_with_generate_file_shp(param=param, created_by_id=current_worker.id, bg_task=bg_task)
 
-    return create_response({"detail": "SUCCESS"})
+    return create_response(data=results)
 
 @router.post("/export/excel")
 async def export_excel(param:BidangParameterDownload|None = None,
