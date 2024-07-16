0ed3157dddbc8336e7563e7dddfe9b4a1791f522
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-07-16 19:38:36 +0700

    bug void invoice & printout memo

--- services/invoice_service.py
+++ services/invoice_service.py
@@ -1,5 +1,7 @@
 
 from fastapi import HTTPException
+
+from fastapi_async_sqlalchemy import db
 from sqlmodel.ext.asyncio.session import AsyncSession
 from models import Invoice, Worker
 from schemas.invoice_sch import InvoiceUpdateSch
@@ -13,7 +15,10 @@ import crud
 
 class InvoiceService:
 
-    async def void(self, obj_current: Invoice, current_worker: Worker, reason: str, db_session: AsyncSession):
+    async def void(self, obj_current: Invoice, current_worker: Worker, reason: str):
+
+        db_session = db.session
+        db_session.autoflush = False
 
         if obj_current.termin.jenis_bayar in [JenisBayarEnum.UTJ, JenisBayarEnum.UTJ_KHUSUS]:
             await self.void_invoice_utj(obj_current=obj_current, db_session=db_session)
@@ -42,7 +47,7 @@ class InvoiceService:
         obj_updated.void_by_id = current_worker.id
         obj_updated.void_at = date.today()
 
-        obj_updated = await crud.invoice.update(obj_current=obj_current, obj_new=obj_updated, updated_by_id=current_worker.id, db_session=db_session, with_commit=False)
+        await crud.invoice.update(obj_current=obj_current, obj_new=obj_updated, updated_by_id=current_worker.id, db_session=db_session, with_commit=False)
 
         # VOID PAYMENT DETAIL
         for dt in obj_current.payment_details:
@@ -54,8 +59,13 @@ class InvoiceService:
             
             await crud.payment_detail.update(obj_current=dt, obj_new=payment_dtl_updated, updated_by_id=current_worker.id, db_session=db_session, with_commit=False)
 
-        # VOID TERMIN APA BILA SEMUA INVOICE YANG ADA DI TERMIN TERSEBUT SUDAH DIVOID
-        invoices_active = await crud.invoice.get_multi_invoice_active_by_termin_id(termin_id=obj_current.termin_id, db_session=db_session)
+        # DELETE INVOICE DETAIL KETIKA INVOICE BUKAN DARI UTJ/UTJ KHUSUS
+        if obj_current.termin.jenis_bayar not in [JenisBayarEnum.UTJ, JenisBayarEnum.UTJ_KHUSUS]:
+            for inv_dtl in obj_current.details:
+                await crud.invoice_detail.remove(id=inv_dtl.id, db_session=db_session, with_commit=False)
+
+         # VOID TERMIN APA BILA SEMUA INVOICE YANG ADA DI TERMIN TERSEBUT SUDAH DIVOID
+        invoices_active = await crud.invoice.get_multi_invoice_active_by_termin_id(termin_id=obj_current.termin_id, invoice_id=obj_current.id, db_session=db_session)
         if len(invoices_active) == 0:
             termin = await crud.termin.get(id=obj_current.termin_id)
             termin_updated = TerminUpdateBaseSch.from_orm(termin)
@@ -64,11 +74,6 @@ class InvoiceService:
             termin_updated.void_by_id = current_worker.id
             termin_updated.void_at = date.today()
             await crud.termin.update(obj_current=termin, obj_new=termin_updated, db_session=db_session, with_commit=False)
-
-        # DELETE INVOICE DETAIL KETIKA INVOICE BUKAN DARI UTJ/UTJ KHUSUS
-        if obj_current.termin.jenis_bayar not in [JenisBayarEnum.UTJ, JenisBayarEnum.UTJ_KHUSUS]:
-            for inv_dtl in obj_current.details:
-                await crud.invoice_detail.remove(id=inv_dtl.id, db_session=db_session, with_commit=False)
 
         try:
             await db_session.commit()
