ac5fe6dbc70c6a1070dce891a84f953267634f31
Ginanjar Sumantri <ginanjar.sumantri@agungsedayu.com>
2024-08-15 07:19:31 +0000

    Merged PR 8848: push

--- services/helper_service.py
+++ services/helper_service.py
@@ -391,6 +391,8 @@ class BundleHelper:
         
         bundle_dts = await crud.bundledt.get_by_bundle_hd_id(bundle_hd_id=bundle_hd_obj.id)
 
+        bundle_dts = await crud.bundledt.get_by_bundle_hd_id(bundle_hd_id=bundle_hd_obj.id)
+
         # Memeriksa apakah dokumen yang dimaksud eksis di bundle detail (bisa jadi dokumen baru di master dan belum tergenerate)
         bundledt_obj_current = next((x for x in bundle_dts if x.dokumen_id == dokumen.id and x.bundle_hd_id == bundle_hd_obj.id), None)
         
