294fa86176bbf6c8da20806aae700e88b5378435
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-08-28 14:47:20 +0700

    Report Pembebasan Detail Project

--- routes/endpoints/report_pembebasan.py
+++ routes/endpoints/report_pembebasan.py
@@ -5,16 +5,18 @@ from fastapi_pagination import Params, Page
 from fastapi_pagination.ext.async_sqlalchemy import paginate
 from fastapi_async_sqlalchemy import db
 from common.rounder import RoundTwo
+from common.enum import StatusReportPembebasanEnum
 
 from models import Worker
-from schemas.report_pembebasan_sch import SummaryProjectSch
-from schemas.response_sch import create_response, GetResponseBaseSch
+from schemas.report_pembebasan_sch import SummaryProjectSch, DetailProjectSch
+from schemas.response_sch import create_response, GetResponseBaseSch, GetResponsePaginatedSch
 
 from services.report_pembebasan_service import ReportPembebasanService
 
 from decimal import Decimal
 from datetime import date
 import crud
+import math
 
 router = APIRouter()
 
@@ -23,3 +25,18 @@ router = APIRouter()
 async def report_pembebasan_summary_project(period_date:date, current_worker:Worker = Depends(crud.worker.get_active_worker)):
     objs = await ReportPembebasanService().summary_project(period_date=period_date)
     return create_response(data=objs)
+
+@router.get("/detail_project", response_model=GetResponsePaginatedSch[DetailProjectSch])
+async def report_pembebasan_detail_project(period_date:date, project_id:UUID, status_pembebasan:StatusReportPembebasanEnum, params: Params=Depends()):
+    
+    objs = await ReportPembebasanService().detail_project(period_date=period_date, project_id=project_id, status_pembebasan=status_pembebasan)
+
+    # SETUP MANUAL PAGINATION
+    start = (params.page - 1) * params.size
+    end = params.page * params.size
+    total_items = len(objs)
+    pages = math.ceil(total_items / params.size)
+
+    data = Page(items=objs[start:end], size=params.size, page=params.page, pages=pages, total=total_items)
+
+    return create_response(data=data)
--- services/report_pembebasan_service.py
+++ services/report_pembebasan_service.py
@@ -1,6 +1,10 @@
 from fastapi_async_sqlalchemy import db
+import sqlalchemy
+
+from common.enum import StatusReportPembebasanEnum
 
 from datetime import date
+from uuid import UUID
 import calendar
 
 
@@ -25,5 +29,44 @@ class ReportPembebasanService:
             result.append(dict(row))
 
         return result
+    
+    async def detail_project(self, period_date:date, project_id:UUID, status_pembebasan:StatusReportPembebasanEnum):
 
-        
+        objs = []
+        db_session = db.session
+
+        today = date.today()
+        is_current_period: bool = True if today.month == period_date.month and today.year == period_date.year else False
+
+        last_day = calendar.monthrange(period_date.year, period_date.month)[1]
+        period_date = date(day=last_day, month=period_date.month, year=period_date.year)
+
+        params = {
+            "cut_off" : period_date,
+            "is_current_period" : is_current_period,
+            "project_ids": [project_id]
+        }
+
+        if status_pembebasan == StatusReportPembebasanEnum.BELUM_BEBAS:
+            query = sqlalchemy.text(f"""SELECT * FROM public._b_report_lili_bidang_belum_bebas(:cut_off, :is_current_period, :project_ids)""").params(**params)
+        elif status_pembebasan == StatusReportPembebasanEnum.BEBAS:
+            query = sqlalchemy.text(f"""SELECT * FROM public._b_report_lili_bidang_bebas(:cut_off, :is_current_period, :project_ids)""").params(**params)
+        elif status_pembebasan == StatusReportPembebasanEnum.BELUM_PETLOK:
+            query = sqlalchemy.text(f"""SELECT * FROM public._b_report_lili_bidang_belum_petlok(:cut_off, :is_current_period, :project_ids)""").params(**params)
+        elif status_pembebasan == StatusReportPembebasanEnum.DEAL:
+            query = sqlalchemy.text(f"""SELECT * FROM public._b_report_lili_bidang_deal(:cut_off, :is_current_period, :project_ids)""").params(**params)
+        elif status_pembebasan == StatusReportPembebasanEnum.DEAL_REKLAMASI:
+            query = sqlalchemy.text(f"""SELECT * FROM public._b_report_lili_bidang_deal_reklamasi(:cut_off, :is_current_period, :project_ids)""").params(**params)
+        elif status_pembebasan == StatusReportPembebasanEnum.KJB:
+            query = sqlalchemy.text(f"""SELECT * FROM public._b_report_lili_bidang_kjb(:cut_off, :is_current_period, :project_ids)""").params(**params)
+        else:
+            query = sqlalchemy.text(f"""SELECT * FROM public._b_report_lili_bidang_relokasi(:cut_off, :is_current_period, :project_ids)""").params(**params)
+
+        response = await db_session.execute(query)
+        rows = response.fetchall()
+
+        result = []
+        for row in rows:
+            result.append(dict(row))
+
+        return result
