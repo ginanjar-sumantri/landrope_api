14d56668cc1dd51930ebe9a5696fa668eb0dc443
ginanjar <ginanjar.sumantri@agungsedayu.com>
2024-09-02 12:41:26 +0700

    pelepasan

--- routes/endpoints/pelepasan.py
+++ routes/endpoints/pelepasan.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, status, Depends, Response, HTTPException, UploadFile
 from fastapi_pagination import Params
-from sqlmodel import select, or_, cast, String
-from models import (PelepasanHeader, PelepasanBidang, Bidang, Worker)
-from schemas.pelepasan_header_sch import (PelepasanHeaderSch, PelepasanHeaderCreateSch, PelepasanHeaderUpdateSch, PelepasanHeaderEditSch, PelepasanHeaderByIdSch)
+from sqlmodel import select, or_, cast, String, and_
+from models import (PelepasanHeader, PelepasanBidang, Bidang, Worker, Planing)
+from schemas.pelepasan_header_sch import (PelepasanHeaderSch, PelepasanHeaderCreateSch, PelepasanHeaderUpdateSch, 
+                                        PelepasanHeaderEditSch, PelepasanHeaderByIdSch, BidangSearchSch)
 from schemas.response_sch import (PostResponseBaseSch, GetResponseBaseSch, DeleteResponseBaseSch, GetResponsePaginatedSch, PutResponseBaseSch, create_response)
 from common.exceptions import (IdNotFoundException)
 from common.exceptions import (IdNotFoundException, DocumentFileNotFoundException)
@@ -23,6 +24,45 @@ async def create(sch: PelepasanHeaderCreateSch, current_worker:Worker = Depends(
 
     response_obj = await crud.pelepasan_header.get(id=new_obj.id, with_select_in_load=True)
     
+    return create_response(data=response_obj)
+
+@router.put("/{id}/edit", response_model=PutResponseBaseSch[PelepasanHeaderSch])
+async def edit(id:UUID, sch:PelepasanHeaderEditSch, current_worker:Worker = Depends(crud.worker.get_active_worker)):
+    
+    """Update a obj by its id"""
+
+    obj_current = await crud.pelepasan_header.get(id=id)
+
+    if not obj_current:
+        raise IdNotFoundException(PelepasanHeader, id)
+    
+    if obj_current.is_lock:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Pelepasan sudah terkunci")
+    
+    obj_updated = await crud.pelepasan_header.edit(obj_current=obj_current, obj_new=sch)
+
+    response_obj = await crud.pelepasan_header.get(id=obj_updated.id, with_select_in_load=True)
+
+    return create_response(data=response_obj)
+
+@router.put("/{id}/update", response_model=PutResponseBaseSch[PelepasanHeaderSch])
+async def update(id:UUID, sch:PelepasanHeaderUpdateSch = Depends(PelepasanHeaderUpdateSch.as_form), 
+                file:UploadFile | None = None, current_worker:Worker = Depends(crud.worker.get_active_worker)):
+    
+    """Update a obj by its id"""
+
+    obj_current = await crud.pelepasan_header.get(id=id)
+
+    if not obj_current:
+        raise IdNotFoundException(PelepasanHeader, id)
+    
+    if obj_current.is_lock:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Pelepasan sudah terkunci")
+    
+    obj_updated = await crud.pelepasan_header.update(obj_current=obj_current, obj_new=sch, file=file, updated_by_id=current_worker.id)
+
+    response_obj = await crud.pelepasan_header.get(id=obj_updated.id, with_select_in_load=True)
+
     return create_response(data=response_obj)
 
 @router.get("/no-page", response_model=GetResponseBaseSch[list[PelepasanHeaderSch]])
@@ -46,7 +86,6 @@ async def get_no_page(keyword:str | None = None, current_worker:Worker = Depends
     objs = await crud.pelepasan_header.get_multi_no_page(query=query)
     
     return create_response(data=objs)
-
 
 @router.get("", response_model=GetResponsePaginatedSch[PelepasanHeaderSch])
 async def get_list(keyword:str | None = None, params:Params = Depends(), current_worker:Worker = Depends(crud.worker.get_active_worker)):
@@ -74,7 +113,6 @@ async def get_list(keyword:str | None = None, params:Params = Depends(), current
     
     return create_response(data=objs)
 
-
 @router.get("/{id}", response_model=GetResponseBaseSch[PelepasanHeaderByIdSch])
 async def get_by_id(id:UUID, current_worker:Worker = Depends(crud.worker.get_active_worker)):
 
@@ -87,86 +125,46 @@ async def get_by_id(id:UUID, current_worker:Worker = Depends(crud.worker.get_act
 
     return create_response(data=obj)
     
-@router.put("/{id}/edit", response_model=PutResponseBaseSch[PelepasanHeaderSch])
-async def edit(id:UUID, sch:PelepasanHeaderEditSch, current_worker:Worker = Depends(crud.worker.get_active_worker)):
+@router.get("/search/bidang", response_model=GetResponsePaginatedSch[BidangSearchSch])
+async def search_bidang(project_id: UUID, desa_id: UUID, pelepasan_header_id: UUID | None = None, keyword:str | None = None, params:Params = Depends(), current_worker:Worker = Depends(crud.worker.get_active_worker)):
     
-    """Update a obj by its id"""
+    """Gets a paginated list objects"""
 
-    obj_current = await crud.pelepasan_header.get(id=id)
+    query = select(Bidang).join(Planing, Bidang.planing_id == Planing.id
+                   ).outerjoin(PelepasanBidang, Bidang.id == PelepasanBidang.bidang_id)
 
-    if not obj_current:
-        raise IdNotFoundException(PelepasanHeader, id)
+    if pelepasan_header_id:
+        query = query.where(
+            or_(PelepasanBidang.pelepasan_header_id == pelepasan_header_id, 
+                PelepasanBidang.id == None,
+                and_(
+                    Planing.project_id == project_id,
+                    Planing.desa_id == desa_id
+                )
+            )
+        )
+    else:
+        query = query.where(
+            and_(
+                    PelepasanBidang.id == None,
+                    Planing.project_id == project_id,
+                    Planing.desa_id == desa_id
+                )
+        )
+
+    if keyword:
+        query = query.filter(or_(
+                        cast(Bidang.id_bidang, String).ilike(f"%{keyword}%"),
+                        cast(Bidang.alashak, String).ilike(f"%{keyword}%")
+                    ))
     
-    if obj_current.is_lock:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Pelepasan sudah terkunci")
+    query = query.filter(Bidang.status == "Bebas").distinct()
+
+    objs = await crud.bidang.get_multi_paginated_ordered(query=query)
     
-    obj_updated = await crud.pelepasan_header.edit(obj_current=obj_current, obj_new=sch)
-
-    response_obj = await crud.pelepasan_header.get(id=obj_updated.id, with_select_in_load=True)
-
-    return create_response(data=response_obj)
+    return create_response(data=objs)
 
 
-@router.put("/{id}/update", response_model=PutResponseBaseSch[PelepasanHeaderSch])
-async def update(id:UUID, sch:PelepasanHeaderUpdateSch = Depends(PelepasanHeaderUpdateSch.as_form), 
-                file:UploadFile | None = None, current_worker:Worker = Depends(crud.worker.get_active_worker)):
-    
-    """Update a obj by its id"""
-
-    obj_current = await crud.pelepasan_header.get(id=id)
-
-    if not obj_current:
-        raise IdNotFoundException(PelepasanHeader, id)
-    
-    if obj_current.is_lock:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Pelepasan sudah terkunci")
-    
-    obj_updated = await crud.pelepasan_header.update(obj_current=obj_current, obj_new=sch, file=file, updated_by_id=current_worker.id)
-
-    response_obj = await crud.pelepasan_header.get(id=obj_updated.id, with_select_in_load=True)
-
-    return create_response(data=response_obj)
-
-
-# @router.get("/print-out/{id}")
-# async def printout(id:str):
-
-#     """Print out"""
-
-#     obj_current = await crud.Peminjaman_header.get(id=id)
-
-#     if not obj_current:
-#         raise IdNotFoundException(PeminjamanHeader, id)
-    
-#     obj_bytes = await PeminjamanHeaderService().generate_printout(obj=obj_current)
-    
-#     headers = {'Content-Disposition': 'inline; filename="Report_Peminjaman.pdf"'}
-
-#     response = Response(obj_bytes.getvalue(), headers=headers, media_type="application/octet-stream")
-#     return response
-    
-
-# @router.get("/export/excel")
-# async def get_report_Peminjaman(periode_date: datetime):
-
-#     """Gets a report"""
-#     base_query = "SELECT * FROM public._a_report_Peminjaman(:periode_date)"
-#     params = {
-#         "periode_date": periode_date
-#     }
-
-#     query = text(base_query).params(**params)
-
-#     objs = await crud.Peminjaman_header.get_sql_function(query=query)
-
-#     headers = ["Category", "Jenis", "Total"]
-#     attributes = ["category", "jenis", "total"]
-
-#     excel_data = await ExportExcelService().generate_excel_report(objs, headers, attributes, report_title="Report Perpanjangan Peminjaman")
-    
-#     return StreamingResponse(excel_data,
-#                              media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
-#                              headers={"Content-Disposition": "attachment; filename=report_Peminjaman.xlsx"})
 
 
 
--- schemas/pelepasan_header_sch.py
+++ schemas/pelepasan_header_sch.py
@@ -3,9 +3,11 @@ from models.pelepasan_header_model import PelepasanHeaderBase, PelepasanHeaderFu
 from schemas.pelepasan_bidang_sch import PelepasanBidangCreateUpdateSch, PelepasanBidangSch
 from common.partial import optional
 from common.as_form import as_form
+from uuid import UUID
+from decimal import Decimal
 
 class PelepasanHeaderCreateSch(PelepasanHeaderBase):
-    pelepasan_bidangs: list[PelepasanBidangCreateUpdateSch]
+    pelepasan_bidangs: list[UUID]
 
 
 class PelepasanHeaderSch(PelepasanHeaderFullBase):
@@ -23,8 +25,15 @@ class PelepasanHeaderByIdSch(PelepasanHeaderFullBase):
     pelepasan_bidangs: list[PelepasanBidangSch]
 
 class PelepasanHeaderEditSch(PelepasanHeaderBase):
-    pelepasan_bidangs: list[PelepasanBidangCreateUpdateSch]
+    pelepasan_bidangs: list[UUID]
 
 @as_form
 class PelepasanHeaderUpdateSch(PelepasanHeaderBase):
     pass
+
+class BidangSearchSch(SQLModel):
+    id: UUID
+    alashak: str | None
+    pemilik_name: str | None
+    group: str | None
+    luas_bayar: Decimal | None
